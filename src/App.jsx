dfb694891fa7118020585cbf3482db81a893dd9b
Luthfi Surya ID <luthfisurya324.id@gmail.com>
2025-04-14 04:27:18 +0700

    Mengedit Sidebar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
     <div className='flex h-screen bg-blue-950 text-gray-100 overflow-hidden'>
 
       {/* BG */}
-      <div className='fixed inset-0 z-0'>
+      <div className='fixed inset-0 z-1'>
         <div className='absolute inset-0 bg-gradient-to-br from-blue-950 via-blue-800 to-blue-950 opacity-80' />
         <div className='absolute inset-0 backdrop-blur-sm' />
       </div>
 
+      <Sidebar />
       <Routes>
         <Route path='/' element={<OverviewPage />} />
         <Route path='/products' element={<ProductsPage />} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,16 @@
+import { BarChart2, ShoppingBag, Users, DollarSign, ShoppingCart, TrendingUp, Settings, Menu } from "lucide-react";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const SIDEBAR_ITEMS = [
-  {
-    name: "Overview",
-    icon: BarChart2,
-    color: "#6366f1",
-    path: "/",
-  },
-  { name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/products" },
-  { name: "Users", icon: Users, color: "#EC4899", href: "/users" },
-  { name: "Sales", icon: DollarSign, color: "#10B981", href: "/sales" },
-  { name: "Orders", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
-  { name: "Analytics", icon: TrendingUp, color: "#3B82F6", href: "/analytics" },
-  { name: "Settings", icon: Settings, color: "#6EE7B7", href: "/settings" },
+  { name: "Overview", icon: BarChart2, color: "#ffffff", path: "/"},
+  { name: "Products", icon: ShoppingBag, color: "#ffffff", href: "/products" },
+  { name: "Users", icon: Users, color: "#ffffff", href: "/users" },
+  { name: "Sales", icon: DollarSign, color: "#ffffff", href: "/sales" },
+  { name: "Orders", icon: ShoppingCart, color: "#ffffff", href: "/orders" },
+  { name: "Analytics", icon: TrendingUp, color: "#ffffff", href: "/analytics" },
+  { name: "Settings", icon: Settings, color: "#ffffff", href: "/settings" },
 ];
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -25,15 +22,30 @@ const Sidebar = () => {
     `}
       animate={{ width: isSidebarOpen ? 256 : 80 }}
     >
-      <div className="h-full bg-blue-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-blue-700">
+      <div className='h-full bg-blue-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-blue-700'>
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit"
+          className='p-2 rounded-full hover:bg-gray-700 hover:bg-opacity-55 transition-colors max-w-fit'
         >
           <Menu size={24} />
         </motion.button>
+        <nav className='mt-8 flex-grow'>
+            {SIDEBAR_ITEMS.map((item, index) => (
+                <Link key={item.href} to={item.href}>
+                    <motion.div
+                    className='flex items-center p-4 text-sm font-medium rounded-lg hover:bg-blue-700 transition-colors mb-2'
+                    >
+                        <item.icon size={20} style={{color: item.color, minWidth: "20px"}} />
+
+                    
+                    </motion.div>
+                </Link>
+            ))}
+
+        </nav>
+
       </div>
     </motion.div>
   );
